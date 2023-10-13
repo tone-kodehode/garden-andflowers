789790e5011adc990f93a8764240a90de2fd4a08
tone-kodehode <tone.kodehode@gmail.com>
2023-10-14 00:20:12 +0200

    url test

--- src/data/bannerList.js
+++ src/data/bannerList.js
@@ -28,7 +28,7 @@ export const bannerList = {
             id: 0,
             type: 'image',
             className: "imgSection",
-            imageUrl: '/tulip1904-804.jpg', 
+            imageUrl: '/ImgBanner/tulip1904-804.jpg', 
             alt: 'Home page, Garden & Flowers',
             title: 'Garden & Flowers',
             topLeftTagline: 'A Greener Everyday Life',
